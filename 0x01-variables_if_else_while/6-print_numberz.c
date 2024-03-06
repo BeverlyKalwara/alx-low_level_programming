2a554d70489ec9011eb42da6714d16e477a83681
BeverlyKalwara <beverlykluvaga@gmail.com>
2024-03-06 06:33:26 -0800

    putchar - prints all single digit numbers of base 10 starting from 0, followed by a new line

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -4,6 +4,9 @@
  * main - Prints all single digit numbers of base 10 starting from 0,
  *        only using putchar and without char variables.
  *
+ * in C singlequoted character literal has numeric value==its ASCII code
+ *      for instance '0' == 48
+ *
  * Return: Always 0.
  */
 
@@ -12,7 +15,9 @@ int main(void)
 	int i;
 
 	for (i = 0; i <= 9; i++)
-		putchar(i);
+	{
+		putchar(i + '0');
+	}
 	putchar('\n');
 
 	return (0);
